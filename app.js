4ac270cf577f7490b29c81c80fdf1774a895fe04
Shivam Shukla <shivam.p.shukla@medtronic.com>
2023-04-01 20:55:14 -0400

    Moved Controllers to different repo

--- app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import HelloController from "./controllers/hello-controller.js"
-import UsersController from "./controllers/users/users-controller.js";
-
-const app = express()
-app.use(express.json())
-HelloController(app)
-UsersController(app)
-app.listen(4000)
