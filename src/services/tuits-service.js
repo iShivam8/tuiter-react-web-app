d93adedd55b2dbfa9b3237952f5764d5489a26aa
Shivam Shukla <shivam.p.shukla@medtronic.com>
2023-04-05 21:51:35 -0400

    A9 Env Variables

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,8 +1,20 @@
 import axios from 'axios';
-// const TUITS_API = 'http://localhost:4000/api/tuits';
-const TUITS_API = 'https://tuiter-node-server-app-zimt.onrender.com/api/tuits';
+//const TUITS_API = 'http://localhost:4000/api/tuits';
+
+// Node API for A8
+//const TUITS_API = 'https://tuiter-node-server-app-zimt.onrender.com/api/tuits';
+
+// Node API for A9
+//const TUITS_API = 'https://tuiter-node-server-app-a9-35a7.onrender.com';
+
 const API_BASE = process.env.REACT_APP_API_BASE;
-//const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = `${API_BASE}/tuits`;
+
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+
+// console.log("DB CONNECTION STRING: " + CONNECTION_STRING);
+// console.log("API BASE: " + API_BASE);
+// console.log("TUITS API: " + TUITS_API);
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
